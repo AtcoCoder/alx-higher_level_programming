1febaa906badebcb3c4b61a4a5253050ce13d827
Omar Jammeh <orms45@gmail.com>
2023-03-11 13:45:27 +0000

    Print list in reversed edge case correction

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    max_idx = len(my_list) - 1
-    for idx in range(max_idx, -1, -1):
-        print("{:d}".format(my_list[idx]))
+    if isinstance(my_list, list):
+        new_list = my_list.copy()
+        new_list.reverse()
+        for element in new_list:
+            print("{:d}".format(element))
