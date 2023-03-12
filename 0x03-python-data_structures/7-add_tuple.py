10d893b553897a9022f283dc75075a2e15c3e8e9
Omar Jammeh <orms45@gmail.com>
2023-03-12 11:30:08 +0000

    Addding 2 tuples, taking care of the edge cases

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,8 +4,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     list_a = [0, 0]
     list_b = [0, 0]
     for idx, num in enumerate(tuple_a):
-        list_a[idx] = num
+        if idx < 2:
+            list_a[idx] = num
     for idx, num in enumerate(tuple_b):
-        list_b[idx] = num
+        if idx < 2:
+            list_b[idx] = num
     new_list = [a + b for a, b in zip(list_a, list_b)]
     return tuple(new_list)
