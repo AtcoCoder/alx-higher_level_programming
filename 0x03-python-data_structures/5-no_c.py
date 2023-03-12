b91342cb49a1c523e9651b4529748f7cdd20c052
Omar Jammeh <orms45@gmail.com>
2023-03-12 00:53:36 +0000

    Remove 'c' and 'C' from my string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    letter_list = [letter for letter in my_string if letter != 'c' and letter != 'C']
+    new_word = "".join(letter_list)
+    return new_word
