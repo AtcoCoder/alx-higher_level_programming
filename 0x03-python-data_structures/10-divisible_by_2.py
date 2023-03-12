e69b2d15d2d911aacc06717721feb8940f14a9cb
Omar Jammeh <orms45@gmail.com>
2023-03-12 17:02:58 +0000

    Find all multiples of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
-    for num in my_list:
-        if num % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
+    new_list = [False if num % 2 else True for num in my_list]
     return new_list
