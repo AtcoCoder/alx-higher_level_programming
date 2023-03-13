6be6a4d984619b3137c305f3f4843096e8fbad39
Omar Jammeh <orms45@gmail.com>
2023-03-13 15:28:12 +0000

    Is the list a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+listint_t *reverse_listint(listint_t **list_head);
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ *
+ * @head: pointer to the pointer to the first node
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = NULL;
+	listint_t *fast = NULL;
+	listint_t *current = NULL;
+	listint_t *second_head = NULL;
+
+	if (*head == NULL)
+		return (1);
+
+	slow = *head;
+	fast = *head;
+	while (1)
+	{
+		if (fast->next == NULL)
+		{
+			second_head = slow->next;
+			slow = NULL;
+			break;
+		}
+		if (fast->next->next == NULL)
+		{
+			second_head = slow->next;
+			slow->next = NULL;
+			break;
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+
+
+	second_head = reverse_listint(&second_head);
+	current = *head;
+	while (current->next != NULL)
+	{
+		if (current->n != second_head->n)
+		{
+			return (0);
+		}
+		current = current->next;
+		second_head = second_head->next;
+	}
+
+	return (1);
+}
+
+/**
+ * reverse_listint - reverses a listint_t list
+ * @list_head: pointer to the pointer to the first node
+ * Return: pointer to the new first node
+ */
+listint_t *reverse_listint(listint_t **list_head)
+{
+	listint_t *current;
+	listint_t *previous = NULL;
+
+	current = *list_head;
+	while ((*list_head)->next != NULL)
+	{
+		*list_head = (*list_head)->next;
+		current->next = previous;
+		previous = current;
+		current = *list_head;
+	}
+	(*list_head)->next = previous;
+	return (*list_head);
+}
