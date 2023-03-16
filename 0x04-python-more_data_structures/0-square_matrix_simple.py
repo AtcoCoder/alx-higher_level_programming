10a45eb1356b625d9386a2ab270f1ebaf563ba5c
Omar Jammeh <orms45@gmail.com>
2023-03-16 02:15:26 +0000

    Squared matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     # create a new matrix from the square of each value of matrix
-    new_matrix = [[row[i] ** 2 for row in matrix] for i in range(len(matrix))]
+    new_matrix = [[num ** 2 for num in matrix[i]] for i in range(len(matrix))]
     # return a new matrix same size as matrix
     return new_matrix
