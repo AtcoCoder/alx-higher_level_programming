5c58ec1ed48fd6b871558aefaceda5ad03b18386
Omar Jammeh <orms45@gmail.com>
2023-03-17 10:49:58 +0000

    Delete keys with a specific value in dict

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
+    """ Takes in a matrix and squares each value of the matrix
+    and returns it as a new matrix """
     return list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
