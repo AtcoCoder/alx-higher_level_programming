ed11dae2eada894052b0544012fa9af948ae971a
Omar Jammeh <orms45@gmail.com>
2023-03-29 01:22:41 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Contains the definition of the square class"""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initializes the data."""
+
+        # private instance attribute
+        self.__size = size
