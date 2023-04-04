f5e8b307286edc04ad57eb2ca72b46747fbf3b2e
Omar Jammeh <orms45@gmail.com>
2023-04-04 15:11:22 +0000

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 """Contains Class Rectangle"""
 
+
 class Rectangle:
     """Defines a rectangle"""
 
     def __init__(self, width=0, height=0):
         """Initialises the instances"""
-        if not isinstance(width, int):
-            raise TypeError("height must be an integer")
-        elif width < 0:
-            raise ValueError("height must be >= 0")
-        elif not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
+        self.raise_if_error(width)
+        self.raise_if_error(height)
         self.__width = width
         self.__height = height
 
@@ -25,10 +20,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Set the width of the rectangle"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
+        self.raise_if_error(value)
         self.__width = value
 
     @property
@@ -39,8 +31,13 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Set the height of the rectangle"""
+        self.raise_if_error(value)
+        self.__height = value
+
+    def raise_if_error(self, value):
+        """Raise the appropiate error
+        if the value is not integer or is less 0"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
