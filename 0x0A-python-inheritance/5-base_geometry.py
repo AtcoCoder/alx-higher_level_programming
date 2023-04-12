016585e131e57619043d2bc8e4af724aca383c1d
Omar Jammeh <orms45@gmail.com>
2023-04-12 01:50:08 +0000

    Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Contains class BaseGeometry"""
+
+
+class BaseGeometry:
+    pass
+
+
+if __name__ == "__main__":
+    bg = BaseGeometry()
+
+    print(bg)
+    print(dir(bg))
+    print(dir(BaseGeometry))
