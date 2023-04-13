36534d8e013ec7f024e853cb7720eab37f927a6a
Omar Jammeh <orms45@gmail.com>
2023-04-13 01:02:07 +0000

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Contains to_json_string function"""
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of
+    an object(string)"""
+    import json
+    return json.dumps(my_obj)
