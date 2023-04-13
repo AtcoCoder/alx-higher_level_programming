d711f8e380e934b11cb72d67a82fd7e0577f0142
Omar Jammeh <orms45@gmail.com>
2023-04-13 14:46:43 +0000

    Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" pascal_triangle function module
+"""
+
+
+def pascal_triangle(n):
+    """
+         returns a list of lists of
+         integers representing
+         the Pascal's triangle
+    """
+    i = 0
+    triangle = [[1]]
+    previous = []
+    while i < n - 1:
+        current_list = [1]
+        for index in range(i):
+            current_list += [previous[index] + previous[index + 1]]
+        previous = current_list
+        previous += [1]
+        triangle.append(previous)
+        i += 1
+    return triangle
