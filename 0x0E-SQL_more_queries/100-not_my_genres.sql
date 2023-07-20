fc7cbcb512aa0f236aa70e668b9c35a0077972a9
Omar Jammeh <orms45@gmail.com>
2023-07-20 12:40:44 +0000

    Updated: 100-not_my_genres.sql
    Sorted result in ascening order by genre name

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,7 +1,7 @@
 -- lists all genres linked to that show Dexter, from the
 -- database hbtn_0d_tvshows.
 
-SELECT tv_genres.name as name
+SELECT tv_genres.name AS name
 FROM tv_genres
 WHERE tv_genres.id
 NOT IN (
@@ -11,3 +11,4 @@ NOT IN (
     ON tv_shows.id=tv_show_genres.show_id
     WHERE tv_shows.title='Dexter'
 )
+ORDER BY name ASC;
