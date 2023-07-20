2da17f451084b5357aae9e22bb812fa3e344b2a1
Omar Jammeh <orms45@gmail.com>
2023-07-20 17:36:28 +0000

    Added: 101-avg_temperatures.sql
    Script that displays the average temperature (Fahrenheit) by city ordered by temp

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,8 @@
+-- Script that displays the average temperature (Fahrenheit) by city ordered by temp(desc)
+
+SELECT
+	city,
+    AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
