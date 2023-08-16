8ff07b66824a3750b7dbd24eadce3711b70b606d
Omar Jammeh <orms45@gmail.com>
2023-08-16 19:08:22 +0000

    Added file: Task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+function secondBiggest (array) {
+  const length = array.length;
+  if (length <= 1) {
+    console.log(0);
+    return;
+  }
+  const newArray = array.map((element) => element);
+  newArray.sort();
+  newArray.reverse();
+  console.log(newArray[1]);
+}
+
+const args = process.argv;
+const length = args.length;
+const array = [];
+let index;
+for (index = 2; index < length; index++) {
+  array.push(args[index]);
+}
+secondBiggest(array);
