1f30a823ac4ac0e94c889d8a0c58ef9bcc680e9f
Omar Jammeh <orms45@gmail.com>
2023-08-16 16:06:45 +0000

    Added Advanced: Task 12

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+const fs = require('fs');
+let fromA = '';
+let fromB = '';
+let toC = '';
+
+fs.readFile(fileA, (err, inputA) => {
+  if (err) {
+    throw err;
+  }
+  fromA = inputA.toString();
+
+  fs.readFile(fileB, (err, inputB) => {
+    if (err) {
+      throw err;
+    }
+    fromB = inputB.toString();
+    toC = fromA + fromB;
+    console.log(toC);
+    fs.writeFile(fileC, toC, (err) => {
+      if (err) throw err;
+      fromB = inputB.toString();
+    });
+  });
+});
