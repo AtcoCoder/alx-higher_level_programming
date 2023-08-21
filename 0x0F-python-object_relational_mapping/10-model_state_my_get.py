c7de469c28c2a9c1b0f72682c43f6f2c5d8a619d
Omar Jammeh <orms45@gmail.com>
2023-08-21 02:14:55 +0000

    Added Task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Sript that prints State objects with the name
+   passed as argument from the database hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    import sys
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+    db_url = "mysql+mysqldb://{}:{}@localhost/{}"
+    engine = create_engine(
+                db_url.format(user, password, database),
+                pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.name == state_name).first()
+    if state is None:
+        print("Not found")
+    else:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
