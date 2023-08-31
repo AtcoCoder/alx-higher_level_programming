9a7d9dbc60e00b27b8b9ce3b670b8f54e8fe8495
Omar Jammeh <orms45@gmail.com>
2023-08-31 13:29:37 -0700

    shellcheck fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Takes in a URL, sends a request to the URL, and display the
 # size of the body of the response
 
-curl -sI $1 | grep Content-Length | cut -d ' ' -f 2
+curl -sI "$1" | grep Content-Length | cut -d ' ' -f 2
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,3 @@
+# Python - Network
+
+This for the student to understand what is HTTP and HTTP requests, response, cookies etc.
