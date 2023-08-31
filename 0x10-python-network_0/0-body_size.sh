cf54ecaace5a70596a4598a245956965b338efca
Omar Jammeh <orms45@gmail.com>
2023-08-31 13:33:41 -0700

    Line count fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to the URL, and display the
-# size of the body of the response
-
+# Takes in a URL, sends a request to the URL, and display the size of the body of the response
 curl -sI "$1" | grep Content-Length | cut -d ' ' -f 2
