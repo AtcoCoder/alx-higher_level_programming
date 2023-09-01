d08e5e3c4348ccca372a0c427e24458555faf027
Omar Jammeh <orms45@gmail.com>
2023-09-01 12:08:41 -0700

    Task 9 fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing you got me!
-curl -so -X PUT "$1" -o /dev/null; printf "You got me!"
+curl -s -X PUT -H "Origin: HolbertonSchool" -d -L "user_id=98" "$1"
