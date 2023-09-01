8f63433c88002f3dd24fb91dcc32bfd6d4068b5d
Omar Jammeh <orms45@gmail.com>
2023-09-01 23:24:30 +0000

    only run as main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 # Fetches 'https://alx-intranet.hbtn.io/status'
-import rurllib.equest
-
-url = 'https://alx-intranet.hbtn.io/status'
+from urllib import request
 
 output = """Body response:
     - type: {}
     - content: {}
     - utf8 content: {}"""
 
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    print(output.format(type(content), content, content.decode('utf-8')))
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with request.urlopen(url) as response:
+        content = response.read()
+        print(output.format(type(content), content, content.decode('utf-8')))
