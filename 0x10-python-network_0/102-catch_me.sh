d779c4d7ef80d1d56005ca881b1f6ec34e0d1bdf
Omar Jammeh <orms45@gmail.com>
2023-09-01 12:10:06 -0700

    fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing you got me!
-curl -s -X PUT -H "Origin: HolbertonSchool" -d -L "user_id=98" "$1"
+curl -s -X PUT -L -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
