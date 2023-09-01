6baf09551528de64535eba0ff1836e2fb00a373d
Omar Jammeh <orms45@gmail.com>
2023-08-31 17:55:40 -0700

    removed pipes

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -si "$1" | grep HTTP/1.1 | cut -d ' ' -f 2 
+curl -s -o /dev/null -w %{http_code} "$1"
