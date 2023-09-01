b639730897428b86f1499b917848ab26384d1ef1
Omar Jammeh <orms45@gmail.com>
2023-09-01 23:32:22 +0000

    import updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """Fetches 'https://alx-intranet.hbtn.io/status'"""
-from urllib import request
-
-output = """Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}"""
+import urllib.request
 
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
-
-    with request.urlopen(url) as response:
+    output = """Body response:
+    - type: {}
+    - content: {}
+    - utf8 content: {}"""
+    with urllib.request.urlopen(url) as response:
         content = response.read()
         print(output.format(type(content), content, content.decode('utf-8')))
