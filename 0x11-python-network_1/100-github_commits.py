b5651aa01602a4523f20d72672cc9b6e2a67a1ca
Omar Jammeh <orms45@gmail.com>
2023-09-02 23:20:59 +0000

    Task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Takes 2 arguments in order to solve this challenge:
+list 10 commits (from the most recent) of the repository 'rails' by the user
+'rails' using the GitHub API. print format: <sha>: <author name> (one by line)
+"""
+import requests
+import sys
+
+repo = sys.argv[1]
+user = sys.argv[2]
+url = "https://api.github.com/repos/{}/{}/commits"
+
+response = requests.get(url.format(repo, user))
+content = response.json()
+
+for i in range(10):
+    commit = content[i]
+    sha = commit['sha']
+    author = commit['commit']['author']['name']
+    print("{}: {}".format(sha, author))
