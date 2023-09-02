114ae52d730910cf00a11a265e73d83e8b7bbafa
Omar Jammeh <orms45@gmail.com>
2023-09-01 17:33:55 -0700

    Task 2 done

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""takes in a URL and an email, sends a POST request to the passed URL with
+the email as a parameter, and displays the body of the response
+(decoded in utf-8)"""
+from urllib import request, parse
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    params = {'email': email}
+    data = parse.urlencode(params)
+    data = data.encode('ascii')
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        content = response.read()
+        print(content.decode('utf-8'))
